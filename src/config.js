887d11732701f244915e4404b5d5adba21e1ecc0
Raúl López <rldona@gmail.com>
2017-06-19 23:03:05 +0200

    Readme changes

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 const config = {
   // Firebase config
   firebase: {
-    apiKey: "AIzaSyDIe9_h-URUmCp4pp464W6yayC6SifNd-4",
-    authDomain: "filmist-react-native.firebaseapp.com",
-    databaseURL: "https://filmist-react-native.firebaseio.com",
-    storageBucket: "filmist-react-native.appspot.com",
-    messagingSenderId: "489986447626"
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    databaseURL: 'databaseURL',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId'
   },
   // MoviesDB config
   moviedb: {
     endPoint: 'http://api.themoviedb.org/3/',
-    apiKey: 'd29e0f4d164566ae95cfb5022b6ef0c0'
+    apiKey: 'API_KEY'
   }
 };
 
